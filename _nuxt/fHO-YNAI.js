ff913cd614651b039d20a1f232de30bd008839ef
axdielljg2025 <normagonzaleznavarro077@gmail.com>
2025-05-17 15:34:17 +0000

    Deploying to gh-pages from @ axdielljg2025/MPS@2750a02bcaf1e9b2798ff3f24531442d2507f587 🚀

--- _nuxt/fHO-YNAI.js
+++
@@ -1,9 +0,0 @@
-import{d as o,b as s,o as m,$ as u}from"./BgxKURV4.js";import{u as r}from"./Bgrev_-Z.js";const i=o({__name:"PmRun",props:{inStack:{type:Boolean,default:!1},script:{},sync:{default:"_pm"},noSync:{type:Boolean}},setup(n){const t=`
-::code-group{${n.inStack?"in-stack":""} ${n.noSync?"":`sync="${n.sync}"`}}
-${r().packageManagers.value.map(e=>{const a=`${e.command}${e.run}${n.script}`;return`\`\`\`bash [${e.name}]
-${a}
-\`\`\`
-`}).join(`
-`)}
-::
-`;return(e,a)=>{const c=u;return m(),s(c,{value:t,class:"[&:not(:first-child)]:mt-5"})}}}),p=Object.assign(i,{__name:"PmRun"});export{p as default};
