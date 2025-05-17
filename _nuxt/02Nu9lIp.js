f15d814a74257cd161e8a2483bd79a9a2d93091b
axdielljg2025 <normagonzaleznavarro077@gmail.com>
2025-05-17 04:44:03 +0000

    Deploying to gh-pages from @ axdielljg2025/MPS@daffc54adefedad3dcff381e0b0a1531f49ee6b3 🚀

--- _nuxt/02Nu9lIp.js
+++
@@ -1,1 +0,0 @@
-import s from"./BAMUsmqe.js";import{d as r,z as u,g as c,a as d}from"./RUureBD_.js";const f=r({name:"Markdown",extends:s,setup(t){const{parent:n}=u(),{between:e,default:a}=c(),o=d(()=>typeof t.unwrap=="string"?t.unwrap.split(" "):["*"]);return{fallbackSlot:a,tags:o,between:e,parent:n}}}),i=Object.assign(f,{__name:"Markdown"});export{i as default};
