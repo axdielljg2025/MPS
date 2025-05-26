d3ae00a379e9fcd0f7e2ec2b3e74a85d45d758e6
axdielljg2025 <normagonzaleznavarro077@gmail.com>
2025-05-26 01:26:14 +0000

    Deploying to gh-pages from @ axdielljg2025/MPS@9e1b5e81bea0ab5708c826c96e6b4f751ab15c51 🚀

---
+++ _nuxt/CoCHfmA7.js
@@ -0,0 +1,9 @@
+import{d as t,b as s,o as m,$ as r}from"./BtTEXtN-.js";import{u}from"./D5PJi4CI.js";const l=t({__name:"PmX",props:{inStack:{type:Boolean,default:!1},command:{},sync:{default:"_pm"},noSync:{type:Boolean}},setup(n){const o=`
+::code-group{${n.inStack?"in-stack":""} ${n.noSync?"":`sync="${n.sync}"`}}
+${u().packageManagers.value.map(a=>{const e=`${a.x}${n.command}`;return`\`\`\`bash [${a.name}]
+${e}
+\`\`\`
+`}).join(`
+`)}
+::
+`;return(a,e)=>{const c=r;return m(),s(c,{value:o,class:"[&:not(:first-child)]:mt-5"})}}}),f=Object.assign(l,{__name:"PmX"});export{f as default};
